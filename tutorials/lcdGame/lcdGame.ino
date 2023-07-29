7625501123f89cb7c1f73bcb3a61156d874f45b4
Nahuel <nahuelmath@gmail.com>
2023-07-29 15:29:44 -0400

    feat: template logic-game control

--- tutorials/lcdGame/lcdGame.ino
+++ tutorials/lcdGame/lcdGame.ino
@@ -1,47 +1,34 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-int address = 0x27;
+int address = 0x20;
 LiquidCrystal_I2C lcd(address, 16, 2);
 int pinPlayer = 6, playerPosition = 0;
 int movePlayer = 0;
 unsigned long prevTime = millis();
 int shootingDelay = 300;
 
+int move(int pos, LiquidCrystal_I2C lcd);
+void shooting(LiquidCrystal_I2C lcd, int playerPosition);
+
 void setup() {
-  lcd.init();
-  Wire.begin();
-  lcd.backlight();
   pinMode(pinPlayer, INPUT);
-  lcd.setCursor(1, 0);
-  lcd.print("#");
   Serial.begin(9600);
+
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-//  movePlayer = digitalRead(pinPlayer);
-//  if (movePlayer) {
-//    playerPosition = move(playerPosition, lcd);
-//    Serial.println(movePlayer);
-//  }
 
-//    unsigned long currentTime = millis();
-//    if (currentTime - prevTime >= shootingDelay) {
-//      shooting(lcd);
-//    }
-//    shooting(lcd);
-  lcd.setCursor(17, 0);
-  lcd.print("-");
-  delay(500);
-  lcd.print(" ");
-  lcd.setCursor(16, 0);
-  lcd.print("-");
-  delay(500);
-  lcd.print(" ");
-  lcd.setCursor(15, 0);
-  lcd.print("-");
-  delay(500);
-  lcd.print(" ");
+  movePlayer = digitalRead(pinPlayer);
+  if (movePlayer) {
+    playerPosition = move(playerPosition, lcd);
+  }
+
+  unsigned long currentTime = millis();
+  if (currentTime - prevTime >= shootingDelay) {
+    shooting(lcd, playerPosition);
+  }
 }
 
 int move(int pos, LiquidCrystal_I2C lcd) {
@@ -61,15 +48,18 @@ int move(int pos, LiquidCrystal_I2C lcd) {
   return 1;
 }
 
-void shooting(LiquidCrystal_I2C lcd) {
+void shooting(LiquidCrystal_I2C lcd, int playerPosition) {
   int bulletPos = random(0, 2); // Escoge una posicion para la bala c/r al lcd, 0 o 1
   for (int i=17; i>=0; i--) {
-    if (i!=17) {
-      lcd.setCursor(i+1, bulletPos);
+    if (i == 0 && bulletPos == playerPosition) {
+      Serial.println("MORISTEE!");
+    }
+    if (i != 17) {
       lcd.print(" ");
     }
     lcd.setCursor(i, bulletPos);
     lcd.print("-");
     delay(800);
+    lcd.setCursor(i, bulletPos);
   }
 }
